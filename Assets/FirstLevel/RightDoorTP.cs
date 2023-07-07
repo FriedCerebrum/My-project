b4e164eee7a2d25fa0a88e6c679db5b495226af0
FriedCerebrum <112276251+FriedCerebrum@users.noreply.github.com>
2023-07-08 00:59:11 +0300

    1

--- Assets/FirstLevel/RightDoorTP.cs
+++ Assets/FirstLevel/RightDoorTP.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +7,8 @@ public class TeleportTrigger : MonoBehaviour
     public GameObject player; // Drag your player object into this slot in the inspector
     public Vector2 teleportLocation; // Set the coordinates where the player will be teleported
     public Text promptText; // Assign your Text object here to display the prompt
+    public GameObject objectToFade; // Object to fade after teleporting
+    public float fadeDuration = 1f; // Duration of the fading process
 
     private bool playerInRange = false;
 
@@ -35,6 +36,29 @@ public class TeleportTrigger : MonoBehaviour
         {
             player.transform.position = teleportLocation; // Teleport player
             promptText.text = ""; // Remove prompt after teleporting
+            StartCoroutine(FadeOutObject()); // Start the fading process
         }
+    }
+
+    private IEnumerator FadeOutObject()
+    {
+        // Make the object visible
+        objectToFade.SetActive(true);
+
+        SpriteRenderer spriteRenderer = objectToFade.GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null) yield break; 
+
+        float fadeSpeed = 1 / fadeDuration;
+        float alpha = 1f;
+        
+        while (alpha > 0)
+        {
+            alpha -= Time.deltaTime * fadeSpeed;
+            spriteRenderer.color = new Color(1, 1, 1, alpha);
+            yield return null;
+        }
+
+        // Completely disable the object
+        objectToFade.SetActive(false);
     }
 }
