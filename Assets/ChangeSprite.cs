fc4b835357a837d18362b7d81b1336a924303a00
FriedCerebrum <112276251+FriedCerebrum@users.noreply.github.com>
2023-07-14 02:28:37 +0300

    sound
    The MegaSuperSound for thunderphone. No one survived among those who said they didn't like it.

--- Assets/ChangeSprite.cs
+++ Assets/ChangeSprite.cs
@@ -4,11 +4,17 @@ public class DestroyRecord : MonoBehaviour
 {
     public Sprite newSprite; // Новый спрайт, который вы хотите использовать.
     private SpriteRenderer spriteRenderer; // Ссылка на SpriteRenderer.
-    
+    private GameObject theMegaSound; // Ссылка на дочерний объект "TheMegaSound".
+
     private void Start()
     {
         // Получить компонент SpriteRenderer.
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Найти дочерний объект "TheMegaSound".
+        theMegaSound = transform.Find("TheMegaSound").gameObject;
+        // Отключить его в начале.
+        theMegaSound.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,6 +27,9 @@ public class DestroyRecord : MonoBehaviour
 
             // Меняем спрайт.
             spriteRenderer.sprite = newSprite;
+
+            // Включаем дочерний объект "TheMegaSound".
+            theMegaSound.SetActive(true);
 
             // Отключаем скрипт.
             this.enabled = false;
